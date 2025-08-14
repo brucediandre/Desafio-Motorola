1d615410ce1cc77edc85bc0edd0555a9255d5f8f
brucediandre <diandre.bruce@ufam.edu.br>
2025-08-14 10:27:29 -0400

    arquivo .sqlite

---
+++ meu_script.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import sqlite3
+import pandas as pd
+
+
+#connectar ao banco
+conn = sqlite3.connect("live.sqlite")
+#listar as tabelas 
+tabelas = pd.read_sql_query (
+    "SELECT name FROM sqlite_master WHERE type='table';", con=conn
+    )
+print("Tabelas:", tabelas)
+cursor = conn.cursor()
+
+# Ler uma tabela específica
+df = pd.read_sql_query("SELECT tabelas * FROM processes2", conn)
+
+conn.close()
